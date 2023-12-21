c0eef515b31e06fe77b2be5e394717645793b45b
esbrinachain <ebrinachain@email.com>
2023-12-21 21:44:19 +0100

    c2

--- dlca/q1/src/app/app.component.ts
+++ dlca/q1/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'q1';
+  title = 'ESBRINA CHAIN - Collaborative Knowledge System';
 }
